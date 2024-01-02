ce45ff390cda5e530ba10513ad1cbd7323f40fcf
Alex <110390382+Fialex1212@users.noreply.github.com>
2024-01-02 20:24:22 +0200

    third version

--- market/market/settings.py
+++ market/market/settings.py
@@ -92,3 +92,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+#EMAILSENDER
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_HOST_USER = "dajngotest@gmail.com"
+EMAIL_HOST_PASSWORD = "jhiijobbjkcnipxw"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
--- market/market_app/views.py
+++ market/market_app/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
+from django.core.mail import send_mail
+from django.conf import settings
+import random
+import string
 
 
 def main(request):
-    return render(request, "market_app/main.html")
+    if request.method == "POST":
+        discout_code = "".join(random.choice(string.ascii_lowercase + string.ascii_uppercase) for i in range(10))
+        message_email = request.POST['message-email']
+
+        send_mail(message_email, f"Hello *name* it's your discount code '{discout_code}'", settings.EMAIL_HOST_USER, [message_email], fail_silently=False, )
+        return render(request, "market_app/main.html")
+    else:
+        return render(request, "market_app/main.html")
 
 
 def products(request):
